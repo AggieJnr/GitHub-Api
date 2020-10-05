149a11ecb9bd1cd669ceb8d578ca5a426051ac49
AggieJnr <aggiemuturi.am@gmail.com>
2020-10-06 01:22:00 +0300

    add setup components

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubAppComponent } from './components/github-app/github-app.component';
+import { GithubProfileComponent } from './components/github-profile/github-profile.component';
+import { GithubProfileCardComponent } from './components/github-profile-card/github-profile-card.component';
+import { GithubProfileDataComponent } from './components/github-profile-data/github-profile-data.component';
+import { GithubReposComponent } from './components/github-repos/github-repos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GithubAppComponent,
+    GithubProfileComponent,
+    GithubProfileCardComponent,
+    GithubProfileDataComponent,
+    GithubReposComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
